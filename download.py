4726becdb2d713c0c201fad9208bb6806d8ea3bb
guoyayong <guoyayong@tigerbrokers.com>
2020-04-19 20:40:20 +0800

    增加爬数据脚本

--- download.py
+++ download.py
@@ -9,34 +9,50 @@ import copy
 
 def download_noteData():
     print "========start"
-    #for i in range(1,2448):
-    for i in range(2459,2482):
+    for i in range(207,2988):
+    #for i in range(2459,2482):
         url = "http://timetag.main.jp/nicoflick/nicoflick.php?req=timetag&id="+str(i)+"&no-json=1&form=1"
         r = requests.get(url)
+        text = r.content.decode("utf-8")
+        html = etree.HTML(text)
+        note_lis = html.xpath("/html/body/textarea/text()")        
         #print r.text
         with open("/Users/tiger/Project/noteData/noteData_"+str(i)+".txt", mode='w') as f:
-            f.write(r.text)
+            try:
+                if note_lis[0]:
+                    f.write(note_lis[0])
+                else:
+                    f.write(r.text)
+            except Exception,e:
+                print e
         f.close()
         
     print "========finish"
 
-def download_noteDataV2(i,url):
-    print "========start"
+def download_noteDataV2(i):
     try:
+        '''
         r = requests.get(url)
+        text = r.content.decode("utf-8")
+        html = etree.HTML(text)
+        note_lis = html.xpath("/html/body/textarea/text()")
         #不应该是写文件，而是直接返回，并且需要时utf-8
-        with open("/Users/tiger/Project/noteData/noteData_"+str(i)+".txt", mode='w') as f:
-            f.write(r.text)
+        '''
+        print "========finish at ",i
+        with open("/Users/tiger/Project/noteData/noteData_"+str(i)+".txt", mode='r') as f:
+            result = f.readlines()
         f.close()
+        return result[0]
     except Exception,e:
         print e
         
         
-    print "========finish"
-
+    
+    #print result[0]
+    #return note_lis[0]
+    
 
 def get_max():
-    print "=========start"
     url = "http://timetag.main.jp/nicoflick/index.php"
     r = requests.get(url)
     text = r.content.decode("utf-8")
@@ -50,14 +66,14 @@ def postSql_getData():
     print "=========start"
     url = "http://timetag.main.jp/nicoflick/index.php"
     m,l = get_max()
-    #body = {
-    #    'music-order': "ORDER BY id DESC LIMIT 0,"+m,
-    #    'level-order': "ORDER BY id DESC LIMIT 0,"+l
-    #}  
     body = {
-        'music-order': "ORDER BY id DESC LIMIT 0,1",
-        'level-order': "ORDER BY id DESC LIMIT 0,1"
-    }    
+        'music-order': "ORDER BY id DESC LIMIT 0,"+m,
+        'level-order': "ORDER BY id DESC LIMIT 0,"+l
+    }  
+    #body = {
+    #    'music-order': "ORDER BY id DESC LIMIT 0,1",
+    #    'level-order': "ORDER BY id DESC LIMIT 0,1"
+    #}    
     r = requests.post(url,data=body)
     text = r.content.decode("utf-8")
     html = etree.HTML(text)
@@ -112,9 +128,9 @@ def postSql_getData():
         level_creator = level_param_lis[3]
         level_des = level_param_lis[4]
         level_speed = level_param_lis[5]
+        #because of their server is weakest , need to get data from txt
         level_note_url = "http://timetag.main.jp/nicoflick/nicoflick.php?req=timetag&id="+level_id+"&no-json=1&form=1"
-        level_note = "Link"
-        #download_noteDataV2(level_id,level_note_url)
+        level_note = download_noteDataV2(level_id)
         level_updatetime = level_param_lis[6]
         level_createtime = level_param_lis[7]        
         
@@ -139,11 +155,12 @@ def uploadToSql(m,l):
     print "===========start"
     db = pymysql.connect("127.0.0.1","root","","flickdb")
     cursor = db.cursor()
-       
+    '''   
     for i in m:
         try:
-            #insert
-            sql1 = "UPDATE music_data SET id = %d ,movie_url = '%s' ,thumbnail_url = '%s' ,title = '%s' ,artist ='%s', movie_length = '%s', tags = '%s' ,update_time = %d ,create_time = %d" % (int(i.get("id")),
+            
+            #update
+            sql1 = "UPDATE music_data SET id = %d ,movie_url = '%s' ,thumbnail_url = '%s' ,title = '%s' ,artist ='%s', notes = '%s', tags = '%s' ,update_time = %d ,create_time = %d" % (int(i.get("id")),
                                                                                                                                                                                                 i.get("movie_url"),
                                                                                                                                                                                                 i.get("thumbnail_url"),
                                                                                                                                                                                                 i.get("title"),
@@ -152,40 +169,63 @@ def uploadToSql(m,l):
                                                                                                                                                                                                 i.get("tags"),
                                                                                                                                                                                                 int(i.get("update_time")),
                                                                                                                                                                                                 int(i.get("create_time")))
-            print sql1
+            
+            #insert
+            sql1 = "INSERT INTO music_data (id  ,movie_url  ,thumbnail_url ,title ,artist , movie_length , tags  ,update_time  ,create_time ) VALUES (%d,'%s','%s','%s','%s','%s','%s',%d,%d)"% (int(i.get("id")),
+                                                                                                                                                                                                i.get("movie_url"),
+                                                                                                                                                                                                i.get("thumbnail_url"),
+                                                                                                                                                                                                i.get("title"),
+                                                                                                                                                                                                i.get("artist"),
+                                                                                                                                                                                                i.get("movie_length"),
+                                                                                                                                                                                                i.get("tags"),
+                                                                                                                                                                                                int(i.get("update_time")),
+                                                                                                                                                                                                int(i.get("create_time")))
+            #print sql1
             cursor.execute(sql1)
             db.commit()
         except Exception,e:
             db.rollback()
             print(e)
-            
+    '''        
     for j in l:
         try:
+            '''
+            #update
+            sql2 = "UPDATE level_data SET id = %d ,movie_url = '%s' ,level = '%s' ,creator = '%s' ,description ='%s', speed = '%s', notes = '%s' ,update_time = %d ,create_time = %d" % (int(j.get("id")),
+                                                                                                                                                                                                j.get("movie_url"),
+                                                                                                                                                                                                j.get("level"),
+                                                                                                                                                                                                j.get("creator"),
+                                                                                                                                                                                                j.get("description"),
+                                                                                                                                                                                                j.get("speed"),
+                                                                                                                                                                                                j.get("notes"),
+                                                                                                                                                                                                int(j.get("update_time")),
+                                                                                                                                                                                                int(j.get("create_time")))
+            '''
             #insert
-            sql1 = "UPDATE music_data SET id = %d ,movie_url = '%s' ,thumbnail_url = '%s' ,title = '%s' ,artist ='%s', movie_length = '%s', tags = '%s' ,update_time = %d ,create_time = %d" % (int(i.get("id")),
-                                                                                                                                                                                                i.get("movie_url"),
-                                                                                                                                                                                                i.get("thumbnail_url"),
-                                                                                                                                                                                                i.get("title"),
-                                                                                                                                                                                                i.get("artist"),
-                                                                                                                                                                                                i.get("movie_length"),
-                                                                                                                                                                                                i.get("tags"),
-                                                                                                                                                                                                int(i.get("update_time")),
-                                                                                                                                                                                                int(i.get("create_time")))
-            print sql1
-            cursor.execute(sql1)
+            sql2 = "INSERT INTO level_data (id  ,movie_url  ,level ,creator ,description , speed , notes  ,update_time  ,create_time ) VALUES (%d,'%s','%s','%s','%s','%s','%s',%d,%d)"%(int(j.get("id")),
+                                                                                                                                                                                                j.get("movie_url"),
+                                                                                                                                                                                                j.get("level"),
+                                                                                                                                                                                                j.get("creator"),
+                                                                                                                                                                                                j.get("description"),
+                                                                                                                                                                                                j.get("speed"),
+                                                                                                                                                                                                j.get("notes"),
+                                                                                                                                                                                                int(j.get("update_time")),
+                                                                                                                                                                                                int(j.get("create_time")))
+            #print sql2
+            cursor.execute(sql2)
             db.commit()
         except Exception,e:
             db.rollback()
-            print(e)        
+            print(e)  
+    
     db.close()
             
     print "===========finish"
     
-    #print r.text    
 if __name__=="__main__":
     #download_noteData()
     postSql_getData()
-    
+    #download_noteDataV2(2)
     
     #查看图片以及视频信息的api for nicovideo
     #http://ext.nicovideo.jp/api/getthumbinfo/sm
